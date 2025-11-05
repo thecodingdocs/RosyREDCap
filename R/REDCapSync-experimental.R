027c51ccae72cc1824712dfd0f1f4632bfd07ce4
brandonerose <brandon.ed.rose@gmail.com>
2025-11-05 10:37:03 -0600

    move complex uploads to RosyREDCap

--- R/REDCapSync-experimental.R
+++ R/REDCapSync-experimental.R
@@ -217,15 +217,15 @@ find_match <- function(x, ref, count_only = FALSE) {
   final_match
 }
 #' @noRd
-combine_project_fields <- function(project) {
-  the_names <- project$transformation$fields$field_name
-  fields <- project$metadata$fields
+combine_project_fields <- function(data_list,transformation) {
+  the_names <- transformation$fields$field_name
+  fields <- data_list$metadata$fields
   if (is.null(the_names)) {
     cli_alert_danger("Nothing to add. Use `add_project_field()`")
     return(fields)
   }
   for (field_name in the_names) {
-    field_row <- project$transformation$fields[which(project$transformation$fields$field_name == field_name), ]
+    field_row <- transformation$fields[which(transformation$fields$field_name == field_name), ]
     form_name <- field_row$form_name
     # if(any(fields$field_name==field_name))stop("field_name already included")
     current_row <- which(fields$field_name == field_name)
@@ -662,4 +662,151 @@ edit_REDCap_while_viewing <- function(project,
       labelled_to_raw_form(project) %>%
       upload_form_to_REDCap(project)
   }
+}
+add_fields_to_data_list <- function(data_list,transformation){
+  metadata <- data_list$metadata
+  named_df_list <- data_list$data
+  the_names <- transformation$fields$field_name
+  has_fields <- !is.null(the_names)
+  original_fields <- metadata$fields
+  the_names_existing <- the_names[which(the_names %in% original_fields$field_name)]
+  the_names_new <- the_names[which(!the_names %in% original_fields$field_name)]
+  field_names <- c(the_names_existing, the_names_new) %>% unique()
+  for (field_name in field_names) {
+    field <- NA
+    row_of_interest <- transformation$fields[which(transformation$fields$field_name == field_name), ]
+    form_name <- row_of_interest$form_name
+    field_func <- transformation$field_functions[[field_name]]
+    environment(field_func) <- environment()
+    if (is_something(field_func)) {
+      if (form_name %in% names(named_df_list)) {
+        field <- field_func(project = data_list,
+                            field_name = field_name,
+                            form_name = form_name)
+      }
+    }
+    if (field_name %in% the_names_existing) {
+      form_old <- named_df_list[[form_name]][[field_name]]
+      if (!identical(field, form_old)) {
+        ref_cols <- metadata$form_key_cols[[form_name]]
+        new <- old <- named_df_list[[form_name]][, c(ref_cols, field_name)]
+        new[[field_name]] <- field
+        form <- REDCapSync:::find_form_diff2(
+          new = new,
+          old = old,
+          ref_cols = ref_cols,
+          view_old = FALSE,
+          message_pass = paste0(form_name, " - ", field_name, ": ")
+        )
+        if (is_something(form)) {
+          transformation$data_updates[[field_name]] <- form
+        }
+      }
+    }
+    if (form_name %in% names(named_df_list)) {
+      named_df_list[[form_name]][[field_name]] <- field
+    }
+  }
+  fields <- combine_project_fields(data_list,transformation)
+  fields$original_form_name <- fields$form_name
+  # fields$form_name <- forms_transformation_original$form_name_remap[match(fields$form_name, forms_transformation_original$form_name)]
+  # fields <- fields[order(match(fields$form_name, forms_transformation$form_name)), ]
+  # new function RosyUtils
+  first <- 1:which(colnames(fields) == "form_name")
+  move <- which(colnames(fields) == "original_form_name")
+  last <- which(colnames(fields) != "original_form_name")[-first]
+  fields <- fields[, c(first, move, last)]
+  metadata$fields <- fields
+  metadata$choices <- REDCapSync:::fields_to_choices(fields)
+  metadata$form_key_cols <- REDCapSync:::get_key_col_list(data_list = data_list)
+  cli_alert_wrap(paste0("Added new fields"), bullet_type = "v")
+  data_list$metadata <- metadata
+  data_list$data <- named_df_list
+  data_list
+}
+#' @title Find the project_import and project differences
+#' @description
+#' This function compares the data in the `project` object (new data) with the
+#' previous or reference data to identify differences. It returns a list of
+#' differences for upload. The function ensures that the new data matches the
+#' structure defined by the metadata and provides warnings when discrepancies
+#' are found.
+#' @param to_be_uploaded a data.frame or list of data.frames to be uploaded
+#' @inheritParams save_project
+#' @param view_old Logical. If TRUE, it will display a preview of the old data
+#' (default is FALSE).
+#' @param n_row_view Numeric. Defines how many rows of the old data to view
+#' (default is 20).
+#' @return A list of differences between the new and old data (`upload_list`).
+#' @details
+#' The function compares the data in `project$data_updates` (new data) with the
+#' current data in the database (`project$data`). If the form names in the new
+#' data do not match the `project$metadata$forms$form_name`, a warning is
+#' issued. The function goes through each table in the new data and compares it
+#' with the old data, recording the differences.
+#'
+#' The `compare` and `to` parameters allow users to specify specific data
+#' choices to compare, though their exact usage will depend on how the function
+#' is fully implemented.
+#' @export
+find_upload_diff <- function(to_be_uploaded,
+                             project,
+                             view_old = FALSE,
+                             n_row_view = 20) {
+  project <- assert_blank_project(project)
+  # if (!all(names(new_list) %in% project$metadata$forms$form_name)) warning("All upload names should ideally match the project form names, `project$metadata$forms$form_name`", immediate. = TRUE)
+  # already_used <- NULL
+  was_df <- is.data.frame(to_be_uploaded)
+  was_list <- is.list(to_be_uploaded)
+  if (!was_df && !was_list) {
+    stop("`to_be_uploaded` must be list of data.frames or a date.frame")
+  }
+  if (was_df) {
+    to_be_uploaded <- list(upload_me = to_be_uploaded)
+  }
+  for (user_name in names(to_be_uploaded)) { # form_name <- names(new_list) %>% sample(1)
+    new <- to_be_uploaded[[user_name]]
+    ref_cols <- project$metadata$raw_structure_cols
+    ref_cols <- ref_cols[which(ref_cols %in% colnames(new))]
+    data_cols <- colnames(new)[which(!colnames(new) %in% ref_cols)]
+    form_names <- field_names_to_form_names(project, data_cols)
+    # if (any(form_names %in% already_used)) {
+    #   stop("REDCapSync will not allow you to upload items from same form multiple times in one loop without refreshing.")
+    # }
+    if (length(form_names) > 1) {
+      if (!all(form_names %in% project$metadata$forms$form_name[which(project$metadata$forms$repeating)])) {
+        stop("Can't have variables in multiple forms in an upload data.frame unless it is a non-repeating form")
+      }
+      stop("Can only upload data from one form at a time.")
+    }
+    relevant_data_cols <- data_cols %>% vec1_in_vec2(
+      form_names_to_field_names(
+        form_names = form_names,
+        project = project
+      )
+    )
+    keep <- c(ref_cols, relevant_data_cols)
+    drop <- data_cols %>% vec1_not_in_vec2(form_names_to_field_names(form_names = form_names, project = project))
+    if (length(drop) > 0) {
+      message("Dropping field_names that aren't part of REDCap metadata: ", toString(drop))
+    }
+    final <- find_form_diff2(
+      new = new[, keep],
+      old = project$data[[form_names]][, keep],
+      ref_cols = ref_cols,
+      message_pass = paste0(user_name, ": "),
+      view_old = view_old,
+      n_row_view = n_row_view
+    )
+    if (was_df) {
+      to_be_uploaded <- final
+    } else {
+      to_be_uploaded[[user_name]] <- final
+    }
+  }
+  if (is_something(to_be_uploaded)) {
+    return(invisible(to_be_uploaded))
+  }
+  message("No upload updates!")
+  invisible(NULL)
 }

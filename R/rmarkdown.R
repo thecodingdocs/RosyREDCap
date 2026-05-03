ab11f5b269700c72e8ac87e5cc2bb871d66b6369
brandonerose <brandon.ed.rose@gmail.com>
2026-05-03 13:44:28 -0500

    dev

--- R/rmarkdown.R
+++ R/rmarkdown.R
@@ -1,9 +1,9 @@
 #' @title rmarkdown_project
-#' `r lifecycle::badge("experimental")`
 #' @inheritParams REDCap_diagram
 #' @param dir_other Character. The directory where the dataset file will be
 #' saved. Default is the `output` folder within the database directory.
 #' @description
+#' `r lifecycle::badge("experimental")`
 #' Generate a rmarkdown PDF from a project object
 #' @details
 #' You will need the [tinytex] and [reticulate] packages. Follow errors during your first use and afterwards it should work

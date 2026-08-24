e1c11b27ecb31f5914249e4f6681c810a85d0340
brandonerose <brandon.ed.rose@gmail.com>
2026-08-24 18:37:33 -0500

    fix errors from updated packages

--- R/app_server.R
+++ R/app_server.R
@@ -480,7 +480,7 @@ app_server <- function(input, output, session) {
   })
   output$choose_survival_status_choice_ <- renderUI({
     choice_df <- values$project$metadata$fields |>
-      dplyr::filter("field_name" == input$choose_survival_status_col) |>
+      dplyr::filter(field_name == input$choose_survival_status_col) |>
       REDCapSync:::fields_to_choices()
     selectizeInput(
       inputId = "choose_survival_status_choice",
@@ -512,7 +512,8 @@ app_server <- function(input, output, session) {
   })
   observeEvent(values$project, {
     message_dev("values$project changed!")
-    if (is_something(values$project)) {
+    if (is_something(values$project) &&
+        is_something(input$transformation_switch)) {
       values$selected_form <- NULL
       values$selected_field <- NULL
       values$selected_instance <- NULL
